2544674bbe4a4e70031294fc2c401124ea7cc3e8
Lantos <lantos@pidor.men>
2021-08-19 19:24:35 +0300

    Create getProduct function, add PHPDoc

--- ajax.php
+++ ajax.php
@@ -5,8 +5,32 @@ class CProducts
 {
     private $db;
 
+    /**
+     * Init DB Connector
+     *
+     * @param DBconnect $db
+     */
     public function __construct(DBconnect $db)
     {
         $this->db = $db;
     }
+
+    /**
+     * Get all products from DataBase
+     *
+     * @param int $count
+     * @return mixed
+     */
+    public function getProducts(int $count = 10)
+    {
+        $stmt = $this->db->stmt_init();
+        $stmt->prepare("SELECT * FROM Products ORDER BY id DESC LIMIT ?");
+        $stmt->bind_param("i", $count);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $products = $result->fetch_all(MYSQLI_ASSOC);
+        return $products;
+    }
 }
+
+
