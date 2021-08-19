c162e0becd4ccaabedb17910849e2587363dd32b
Lantos <lantos@pidor.men>
2021-08-19 17:15:35 +0300

    Create CProducts class and modify table

--- ajax.php
+++ ajax.php
@@ -1,1 +1,12 @@
 <?php
+require_once __DIR__ . '/db.php';
+
+class CProducts
+{
+    private $db;
+
+    public function __construct(DBconnect $db)
+    {
+        $this->db = $db;
+    }
+}
--- index.php
+++ index.php
@@ -6,10 +6,32 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Products</title>
+    <style>
+        table {
+            border-collapse: collapse;
+        }
+
+        td {
+            border: 1px solid #fff;
+            height: 20px;
+        }
+    </style>
 </head>
 <body>
 <table>
+    <thead>
+    <tr>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+    </tr>
+    </thead>
+    <tbody>
 
+    </tbody>
 </table>
 
 <br>
